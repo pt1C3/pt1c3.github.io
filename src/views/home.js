f3b20b0385045b4b17d6064e03b128a43855f964
pt1C3 <rafaelsilva1c3@gmail.com>
2025-02-10 16:14:03 +0000

    bug fixes, little rework

--- src/views/home.js
+++ src/views/home.js
@@ -5,6 +5,7 @@ import Logo from '../images/Logo.svg';
 import Profile from '../images/me.png';
 import { Helmet } from 'react-helmet';
 import TextPressure from '../blocks/TextAnimations/TextPressure/TextPressure';
+import DecryptedText from '../blocks/TextAnimations/DecryptedText/DecryptedText';
 import ShinyText from '../blocks/TextAnimations/ShinyText/ShinyText';
 import Waves from '../blocks/Backgrounds/Waves/Waves';
 import checkTouchDevice from '../utils/checkTouch';
@@ -41,22 +42,23 @@ export default function Home() {
 
         <Link to="/projects">projects</Link>
         <Waves
-        lineColor={lineColor}
-        backgroundColor="var(--gray-1-color)"
-        waveSpeedX={0.01}
-        waveSpeedY={0.01}
-        waveAmpX={64}
-        waveAmpY={20}
-        friction={0.9}
-        tension={0.01}
-        maxCursorMove={0}
-        xGap={12}
-        yGap={24}
-      />
+          lineColor={lineColor}
+          backgroundColor="var(--gray-1-color)"
+          waveSpeedX={0.01}
+          waveSpeedY={0.01}
+          waveAmpX={64}
+          waveAmpY={20}
+          friction={0.9}
+          tension={0.01}
+          maxCursorMove={0}
+          xGap={12}
+          yGap={24}
+        />
       </section>
       <section>
         <div>
-
+          <br/>
+          ...wip...
         </div>
       </section>
 
